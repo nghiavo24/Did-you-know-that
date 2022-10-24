c34e11e77e2bed70ae2a7c100ce6e12280a82441
nghiavo24 <nghia.vo1452@gmail.com>
2022-10-23 19:10:23 -0500

    functionalities added, need to meet MVP goals #1 and #3

--- version2.js
+++ version2.js
@@ -10,40 +10,134 @@
 // - restarts the game loop if there are more questions
 // - ends game if there are no more questions
 
-function gameStart () {
-    let score = 0;
+const startButton = document.querySelector('.start-btn')
+const nextButton = document.querySelector('.next-btn')
+const questionContainerElement = document.querySelector('#question-container')
+const questionElement = document.querySelector('#question')
+const answerButtons = [
+    document.getElementById('answer1'),
+    document.getElementById('answer2'),
+    document.getElementById('answer3')
+]
+const score = document.getElementById('score');
+const scoreHeader = document.getElementById('score-header');
+var playerAnswer = null;
+var playerAnswered = false;
+var amountWrong = 0
 
-    for(let j = 0; j <questions.length; j++){
-        let QuestionObject = questions[j];
+startButton.addEventListener('click', gameStart)
+
+async function gameStart () {
+    //  hide/unhide html
+    startButton.classList.add("hide")
+    questionElement.classList.remove("hide")
+    scoreHeader.classList.remove("hide")
+
+    // add listeners to answer buttons to get the player answer
+    for(let i =0; i < answerButtons.length; i++){
+        answerButtons[i].addEventListener('click', () => setAnswer(i))
+        answerButtons[i].classList.remove("hide")
+    }
+
+    let playerScore = 0;
+
+    // gameplay loop
+    for(let j = 0; j <marvel_questions.length; j++){
+        let QuestionObject = marvel_questions[j];
         let question = QuestionObject.question
         console.log("-------------------");
         console.log(question);
+        
+        // show the question
+        questionElement.innerText = question
 
+        // show the answers
         let answers = QuestionObject.answers;
         for( let i = 0; i < answers.length; i++) {
-            let possibleAnswer = answers[i]; // {text: 'Natasha Romanoff', correct: true}
-            console.log(`${i+1}: ${possibleAnswer.text}`) 
+            let possibleAnswer = answers[i];
+            console.log(`${i+1}: ${possibleAnswer.text}`)
+            answerButtons[i].innerText = possibleAnswer.text
         }
-        let playerAnswer = randomIntFromInterval(0,2)
-        console.log(`You answered: ${answers[playerAnswer].text}`)
 
-        if(answers[playerAnswer].correct === true){
+        
+        // wait for player answer
+        await waitUserInput()
+        console.log(`You answered:`)
+        console.log(playerAnswer)
+
+        // add score and change background color for wrong/right answer
+        if(playerAnswer != null && answers[playerAnswer].correct === true){
             console.log("Thats Correct!")
-            score +=1;
+            document.body.className = 'correct'
+            playerScore += 1;
         }else {
             console.log("Thats Not Correct!")
+            document.body.className = 'wrong'
+            amountWrong += 1
         }
 
-        console.log(`Your score is: ${score}/${questions.length}`)
+        // add change button colors for wrong/right answer
+        for( let i = 0; i < answers.length; i++) {
+            if(answers[i].correct == true){
+                answerButtons[i].classList.add('correct')
+            }else{
+                answerButtons[i].classList.add('wrong')
+            }
+        }
 
+        // show updated score
+        console.log(`Your score is: ${playerScore}/${marvel_questions.length}`)
+        score.innerText = `${playerScore}/${marvel_questions.length}`
+
+        await sleep(1000)
+
+        if(amountWrong >= 3){
+            //TODO: Add logic for ending game when more than three wrong answers
+        }
+
+        // reset player answer and colors
+        playerAnswer = null
+        document.body.className = ''
+        for( let i = 0; i < answers.length; i++) {
+            if(answers[i].correct == true){
+                answerButtons[i].classList.remove('correct')
+            }else{
+                answerButtons[i].classList.remove('wrong')
+            }
+        }
     }
+
+    // hide/unhide html
+    for(let i =0; i < answerButtons.length; i++){
+        answerButtons[i].classList.add("hide")
+    }
+    questionElement.classList.add("hide")
+    startButton.classList.remove("hide")
 }
 
+// sets the player answer when an answer button is clicked
+function setAnswer(answer_index){
+    playerAnswer = answer_index;
+    playerAnswered = true
+}
+
+// waits in an infitie loop until playerAnswered is changed to true  (playerAnswered is set to true by setAnswer())
+async function waitUserInput() {
+    while (playerAnswered == false) await sleep(50); // pauses script
+    playerAnswered = false; // reset var
+}
+
+// gets random number from range
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const questions = [
+// stops program for an amount of time in miliseconds
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+// const dc_questions = []
+const marvel_questions = [
     {
         question: 'What is Black Widow real name?',
         answers: [
@@ -125,5 +219,3 @@ const questions = [
         ]
     }
 ]
-
-gameStart()
